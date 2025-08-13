f5dc8ae666225c208c9745341bffdcd50f899e53
CandelyStack <candely1charifa@gmail.com>
2025-08-13 18:09:37 +0100

    Required Files Backend & Setup Styling index

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -2,9 +2,14 @@ import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head />
-      <body className="antialiased">
+    <Html>
+      <Head>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Charm:wght@400;700&family=Jost:wght@300;400;500;600;700&family=Oregano&family=Roboto:wght@300;400;500;700;900&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+      <body>
         <Main />
         <NextScript />
       </body>
